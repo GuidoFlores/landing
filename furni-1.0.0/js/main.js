a42661ee7b8c35538491ea8c374d57821fe1824b
Kevin Mejia <142460056+Valery1302@users.noreply.github.com>
2024-06-05 09:29:28 -0500

    update

--- furni-1.0.0/js/main.js
+++ furni-1.0.0/js/main.js
@@ -1,7 +1,9 @@
 let loaded = ( eventLoaded ) => {
   
-    window.alert("landing page loaded");
-    console.log( eventLoaded );
-    debugger;
+  window.alert("landing page loaded");
+  console.log( eventLoaded );
+  debugger;
   
-  }
+}
+
+window.addEventListener("DOMContentLoaded", loaded);
