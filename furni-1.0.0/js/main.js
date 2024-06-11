aa206dc6de2d0dd8d0df3046fc1d6ae4a181bf25
Guido Flores <gjflores@espol.edu.ec>
2024-06-10 23:17:17 -0500

    update

--- furni-1.0.0/js/main.js
+++
@@ -1,9 +0,0 @@
-let loaded = ( eventLoaded ) => {
-  
-  window.alert("landing page loaded");
-  console.log( eventLoaded );
-  debugger;
-  
-}
-
-window.addEventListener("DOMContentLoaded", loaded);
