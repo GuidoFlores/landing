245a42907732227b388d68c10bbf013d75068900
Guido Flores <gjflores@espol.edu.ec>
2024-06-15 18:11:43 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,42 +1,96 @@
 let loaded = (eventLoaded) => {
 
-    window.alert("landing page loaded");
+    /*window.alert("landing page loaded");
     console.log(eventLoaded);
-    debugger;
+    debugger;*/
 
     let myform = document.getElementById('formulario');
 
-    myform.addEventListener('submit', (eventSubmit) => {
+    let urlBase = 'https://proyecto1-3a9b5-default-rtdb.firebaseio.com/collection.json'
+
+    myform.addEventListener('submit', async (eventSubmit) => {
 
         eventSubmit.preventDefault();
 
-        let nombreValue = nombre.value;
+        let nombreValue = myform.nombre.value.trim();
+        let correoValue = myform.correo.value.trim();
 
         // Validación del contenido del input 
 
-        if (nombreValue.length == 0) {
+        if (nombreValue === '' || correoValue === '') {
 
-            nombre.focus()
-
-            alert('Ingrese un texto válido')
+            alert('por favor ingrese todos los campos')
 
             return;
         }
 
-        let correoValue = correo.value;
+        try{
+            let respuesta = await fetch(urlBase, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({'email': correoValue, 'nombre': nombreValue  })
+            });
 
-        if(correoValue.length == 0){
+            if(!respuesta.ok){
+                throw new Error('Hubo un problema al enviar los datos.');
+            }
+
+            window.location.href = 'index.html'
+        }catch(error){
+            console.error('Error:', error);
+            alert('Hubo un problema al enviar los datos.');
+        }
+
+        /*if(correoValue.length == 0){
 
             correo.focus()
 
             alert('Ingrese un correo válido')
 
             return;
-        }
+        }*/
 
+        
         debugger;
+        
 
-    })
+    });
+
+    
 }
 
+
+async function obtenerDatos() {
+    const url = "https://proyecto1-3a9b5-default-rtdb.firebaseio.com/collection.json"; // Reemplaza con la URL real de la API o recurso
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+    console.error("Error:", respuesta.status);
+    return;
+    }
+    const datos = await respuesta.json();
+    console.log(datos); // Procesar o mostrar los datos obtenidos
+
+    const tableBody = document.getElementById('tablebody');
+        for (const key in datos) {
+            if (datos.hasOwnProperty(key)) {
+                const obj = datos[key];
+                const categoria = obj.nombre
+                const conteo = obj.email
+                let template = `
+                    <tr>
+                        <td>${categoria}</td>
+                        <td>${conteo}</td>
+                    </tr>
+                `;
+                tableBody.innerHTML += template;
+            }
+        }
+    }
+
+
 window.addEventListener("DOMContentLoaded", loaded);
+obtenerDatos()
+
+
